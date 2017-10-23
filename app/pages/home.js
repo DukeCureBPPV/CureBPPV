e58b48bb5f4d8c80dc991a8c8fd829d6b6b290f2
Yilin Gao <yilin.gao@duke.edu>
2017-10-23 17:55:09 -0400

    #28 extract video player to a separate component, restore HomePage to stateless

--- app/pages/home.js
+++ app/pages/home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Button, Text, View, Linking, Animated, TouchableWithoutFeedback } from 'react-native';
-import Video from 'react-native-video';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from 'react-native-slider';
-import sampleVideo from '../../assets/video/sample.mp4';
+import { StyleSheet, Button, Text, View, Linking } from 'react-native';
 import * as navActions from '../navigation/actions';
 import * as appActions from '../actions';
+import VideoPlayer from './video-player';
+import sampleVideo from '../../assets/video/sample.mp4';
 
 const styles = StyleSheet.create({
   container: {
@@ -33,172 +31,51 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-  videoBox: {
-    width: 300,
-    height: 230,
-    justifyContent: 'center',
-  },
-  video: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  controls: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    height: 30,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    position: 'absolute',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  duration: {
-    color: '#FFF',
-    marginLeft: 15,
-  },
 });
 
 const SURVEY_URL = 'https://redcap.duke.edu/redcap/surveys/?s=YDW8TRLKPJ';
 
-// format passed time in seconds into mins:seconds
-function secondsToTime(time) {
-  return `${Math.floor(time / 60)}:${time % 60 < 10 ? 0 : ''}${time % 60}`;
-}
+const HomePage = ({ setTreatmentSide, goTo }) => (
+  <View style={styles.container} >
+    <View style={styles.section}>
+      <Text style={styles.title}>Cure BPPV</Text>
+      <Text style={styles.description}>
+        Once you are diagnosed with BPPV,
+        this app guides you with self-treatment.
+      </Text>
+    </View>
 
-class VideoPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paused: false, // if the video is paused or not
-      progress: 0, // the percentage of the video played, [0, 1]
-      duration: 0, // the total time of the video
-    };
-    // use this.player.seek(time) when we need to change the position of the video
-    this.player = null;
-  }
+    <VideoPlayer
+      source={sampleVideo}
+    />
 
-  handleLoad(meta) {
-    this.setState({
-      duration: meta.duration,
-    });
-  }
-
-  handleProgress(progress) {
-    this.setState({
-      progress: progress.currentTime / this.state.duration,
-    });
-  }
-
-  handleEnd() {
-    this.setState({
-      paused: true,
-    });
-  }
-
-  handlePressPlayPauseButton() {
-    if (this.state.progress >= 1) {
-      this.player.seek(0);
-    }
-
-    this.setState({
-      paused: !this.state.paused,
-    });
-  }
-
-  handlePressProgress(e) {
-    const position = e.nativeEvent.locationX;
-    const progress = position * (1 / 250) * this.state.duration;
-    this.player.seek(progress);
-  }
-
-  handleSlideProgress(percentage) {
-    this.player.seek(percentage * this.state.duration);
-  }
-
-  render() {
-    return (
-      <View style={styles.container} >
-        <View style={styles.section}>
-          <Text style={styles.title}>Cure BPPV</Text>
-          <Text style={styles.description}>
-            Once you are diagnosed with BPPV,
-            this app guides you with self-treatment.
-          </Text>
-        </View>
-
-        <View style={styles.videoBox}>
-          <Video
-            style={styles.video}
-            source={sampleVideo}
-            paused={this.state.paused}
-            resizeMode="contain"
-            onLoad={meta => this.handleLoad(meta)}
-            onProgress={progress => this.handleProgress(progress)}
-            onEnd={() => this.handleEnd()}
-            ref={(ref) => { this.player = ref; }}
-          />
-
-          <Animated.View style={styles.controls}>
-            <TouchableWithoutFeedback onPress={() => this.handlePressPlayPauseButton()}>
-              <Icon
-                name={!this.state.paused ? 'pause' : 'play'}
-                size={20}
-                color="#FFF"
-              />
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={e => this.handlePressProgress(e)}>
-              <View>
-                <Slider
-                  value={this.state.progress}
-                  onValueChange={value => this.handleSlideProgress(value)}
-                  thumbTouchSize={{ width: 50, height: 50 }}
-                  color="#FFF"
-                  unfilledColor="rgba(255,255,255,.5)"
-                  borderColor="#FFF"
-                  width={200}
-                  height={10}
-                />
-              </View>
-            </TouchableWithoutFeedback>
-            <Text style={styles.duration}>
-              {secondsToTime(Math.floor(this.state.progress * this.state.duration))}
-            </Text>
-          </Animated.View>
-        </View>
-
-        <View style={styles.section}>
-          <Text>Which side for treatment:</Text>
-          <View style={styles.sideChoicesBox}>
-            <Button
-              title="left"
-              onPress={() => {
-                this.props.setTreatmentSide('left');
-                this.props.goTo('Prepare');
-              }}
-            />
-            <Button
-              title="right"
-              onPress={() => {
-                this.props.setTreatmentSide('right');
-                this.props.goTo('Prepare');
-              }}
-            />
-          </View>
-        </View>
-        <View style={styles.section}>
-          <Button
-            title="Survey"
-            onPress={() => { Linking.openURL(SURVEY_URL); }}
-          />
-        </View>
+    <View style={styles.section}>
+      <Text>Which side for treatment:</Text>
+      <View style={styles.sideChoicesBox}>
+        <Button
+          title="left"
+          onPress={() => {
+            setTreatmentSide('left');
+            goTo('Prepare');
+          }}
+        />
+        <Button
+          title="right"
+          onPress={() => {
+            setTreatmentSide('right');
+            goTo('Prepare');
+          }}
+        />
       </View>
-    );
-  }
-}
+    </View>
+    <View style={styles.section}>
+      <Button
+        title="Survey"
+        onPress={() => { Linking.openURL(SURVEY_URL); }}
+      />
+    </View>
+  </View>
+);
 
 const mapStateToProps = () => ({});
 
@@ -211,4 +88,4 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(VideoPage);
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
